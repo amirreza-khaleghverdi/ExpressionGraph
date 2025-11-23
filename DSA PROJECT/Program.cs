eac4959a63b1234da2a7f4b05c1af6f8b9289276
amirreza khaleghverdi <amirrezakhaleghverdi3@gmail.com>
2025-11-23 19:01:18 +0330

    fixing and cleaning some errors

---
+++ DSA PROJECT/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DSA_PROJECT;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string normalstr = "";
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter input: ");
+                string input = Console.ReadLine();
+                normalstr = Normalizer.Normalizer.Normalize(input);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error ==> " + ex.Message);
+                Console.WriteLine("Please Try Again.\n");
+                // Thread.Sleep(1500);
+                // Console.Clear();
+            }
+        }
+        Console.WriteLine(normalstr);
+    }
+}
+
