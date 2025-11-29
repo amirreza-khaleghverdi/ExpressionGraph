dc9d7aa2c5debfb0a2efb6b1b598acfd04c59f30
amirreza khaleghverdi <amirrezakhaleghverdi3@gmail.com>
2025-11-29 03:35:19 +0330

    infix to postfix added

--- DSA PROJECT/Program.cs
+++ DSA PROJECT/Program.cs
@@ -9,8 +9,7 @@
                 Console.Write("enter input: ");
                 string? input = Console.ReadLine();
                 string postfix = Expression_Builder.Expression_Builder.InToPost(input);
-                double result = Expression_Builder.Expression_Builder.evalute(postfix);
-                Console.WriteLine($"the final result is {result}");
+
             }
             catch (Exception ex)
             {
