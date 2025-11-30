c2a01a673505fb2f9001293e64376fe99765b2b4
amirreza khaleghverdi <amirrezakhaleghverdi3@gmail.com>
2025-11-30 15:20:34 +0330

    completing the tree and traversal

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Expression_Builder;
+﻿using Expression_Tree;
+using Evaluator;
 
 class Program
 {
@@ -9,33 +10,20 @@ class Program
             Console.Write("Enter infix expression: ");
             string? infix = Console.ReadLine();
 
-            // STEP 1 — Tokenize (your teammate will replace this)
+
             List<string> tokens = Tokenize(infix);
 
-            // STEP 2 — Parse into expression tree
-            Parser parser = new Parser(tokens);
-            node root = parser.ParseExpression();
 
-            // STEP 3 — Evaluate the expression (postfix tree evaluation)
-            double result = Evaluator.Evaluate(root);
+            expression_tree parser = new expression_tree(tokens);
+            node root = parser.ParseAddSub();
 
-            // STEP 4 — Show results
-            Console.WriteLine("\nExpression Tree:");
-            PrintTree(root, "");
+
+            double result = evaluator.Evaluate(root);
+
 
             Console.WriteLine($"\nResult = {result}");
         }
 
-    }
-
-    static void PrintTree(node n, string indent)
-    {
-        if (n == null) return;
-
-        Console.WriteLine(indent + n.Value);
-
-        PrintTree(n.Left, indent + "  ");
-        PrintTree(n.Right, indent + "  ");
     }
 
     static List<string> Tokenize(string s)
---
+++ evaluator.cs
@@ -0,0 +1,45 @@
+using Expression_Tree;
+
+namespace Evaluator;
+
+public static class evaluator
+{
+    public static double Evaluate(node root)
+    {
+        if (root == null)
+            throw new Exception("Invalid expression tree");
+
+        return EvaluatePostfix(root);
+    }
+
+    private static double EvaluatePostfix(node root)
+    {
+        if (root.Left == null && root.Right == null)
+        {
+            return double.Parse(root.Value);
+        }
+
+        if (root.Value == "u-")
+        {
+            double val = EvaluatePostfix(root.Left);
+            return -val;
+        }
+
+        double left = EvaluatePostfix(root.Left);
+        double right = EvaluatePostfix(root.Right);
+
+        switch (root.Value)
+        {
+            case "+": return left + right;
+            case "-": return left - right;
+            case "*": return left * right;
+            case "/": return left / right;
+
+            case "^": return Math.Pow(left, right);
+            case "√": return Math.Pow(right, 1.0 / left);
+
+            default:
+                throw new Exception("Unknown operator: " + root.Value);
+        }
+    }
+}
