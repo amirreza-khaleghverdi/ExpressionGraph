4d8386884f12170234d55b43d0c09c0fed9ce7f3
amirreza khaleghverdi <amirrezakhaleghverdi3@gmail.com>
2025-12-04 20:02:14 +0330

    new gui version

---
+++ Views/Window2.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPage1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow homepage= new();
+            homepage.Show();
+            this.Close();
+        }
+
+        private void GoPage2_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 precedence = new();
+            precedence.Show();
+            this.Close();
+        }
+
+        private void GoPage3_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 variable = new();
+            variable.Show();
+            this.Close();
+        }
+
+    }
+}
