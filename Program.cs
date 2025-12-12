4078a5a43444a2eb2593e766b30f199f197c1937
Lenovo <Lenovo@DESKTOP-HDT63HQ>
2025-12-12 13:28:46 +0330

    new main

--- Program.cs
+++ Program.cs
@@ -8,93 +8,98 @@ class Program
     {
         while (true)
         {
-            //Console.WriteLine("choose your option");
-            //Console.WriteLine("1.normal");
-            //Console.WriteLine("2.add variable");
-            //Console.WriteLine("3.change priority opertor ");
-            //int n = int.Parse(Console.ReadLine());
-            int n = 1;
-            if (n == 1)
-            {
-                try
+            Console.WriteLine("Choose Your Option");
+            Console.WriteLine("1.Default Calculate ");
+            Console.WriteLine("2.Add Variable");
+            Console.WriteLine("3.Change Priority Operator");
+            int n = int.Parse(Console.ReadLine());
+            Thread.Sleep(100);
+            Console.Clear();
+            switch (n){
+            case 1:
                 {
-                    Console.Write("Enter Input : ");
-                    string? infix = Console.ReadLine();
-
-                    string Normalize = Preprocess.Normalize(infix);
-                    List<string> tokenize = Tokenizer.Tokenize(Normalize);
-                    foreach (var x in tokenize) Console.Write(x + " ");
-                    Console.WriteLine();
-                    expression_tree parser = new expression_tree(tokenize);
-                    node root = parser.Parse();
-                    double result = evaluator.Evaluate(root);
-                    Console.WriteLine("\nResult :" + result.ToString("F3"));
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("==> " + ex.Message);
-                }
-            }
-            if (n == 2)
-            {
-                try
-                {
-                    Console.WriteLine("Enter Count of variable: ");
-                    int m = int.Parse(Console.ReadLine());
-                    List<Tuple<string, double>> vars = new List<Tuple<string, double>>();
-                    for (int i = 0; i < m; i++)
+                    try
                     {
-                        Console.Write("Enter name: ");
-                        string name = Console.ReadLine();
-                        Console.Write("Enter value: ");
-                        string inputt = Console.ReadLine();
-                        string normalize = Preprocess.Normalize(inputt);
-                        List<string> tokenizee = Tokenizer.Tokenize(normalize);
-                        expression_tree parser = new expression_tree(tokenizee);
-                        node roott = parser.Parse();
-                        double resultt = evaluator.Evaluate(roott);
-                        double value = resultt;
-                        vars.Add(Tuple.Create(name, value));
+                        Console.Write("Enter Input : ");
+                        string? infix = Console.ReadLine();
+
+                        string Normalize = Preprocess.Normalize(infix);
+                        List<string> tokenize = Tokenizer.Tokenize(Normalize);
+                        foreach (var x in tokenize) Console.Write(x + " ");
+                        expression_tree parser = new expression_tree(tokenize);
+                        node root = parser.Parse();
+                        double result = evaluator.Evaluate(root);
+                        Console.WriteLine("\nResult :" + result.ToString("F3"));
                     }
-                    Console.WriteLine("Now Enter your Input for calculate: ");
-                    string input = Console.ReadLine();
-                    string Normalize = Preprocess.Normalize(input);
-                    List<string> tokenize = Tokenizer.Tokenize(Normalize);
-                    expression_tree tree = new expression_tree(tokenize);
-                    node root = tree.Parse();
-                    double result = evaluator.Evaluate(root, vars);
-                    Console.WriteLine("Result = " + result);
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("==> " + ex.Message);
+                    }
                 }
-                catch (Exception ex)
+                break;
+            case 2:
                 {
-                    Console.WriteLine(" ==> " + ex.Message);
+                    try
+                    {
+                        Console.WriteLine("Enter Count of variable: ");
+                        int m = int.Parse(Console.ReadLine());
+                        List<Tuple<string, double>> vars = new List<Tuple<string, double>>();
+                        for (int i = 0; i < m; i++)
+                        {
+                            Console.Write("Enter name: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Enter value: ");
+                            string inputt = Console.ReadLine();
+                            string normalize = Preprocess.Normalize(inputt);
+                            List<string> tokenizee = Tokenizer.Tokenize(normalize);
+                            expression_tree parser = new expression_tree(tokenizee);
+                            node roott = parser.Parse();
+                            double resultt = evaluator.Evaluate(roott);
+                            double value = resultt;
+                            vars.Add(Tuple.Create(name, value));
+                        }
+                        Console.WriteLine("Now Enter your Input for calculate: ");
+                        string input = Console.ReadLine();
+                        string Normalize = Preprocess.Normalize(input);
+                        List<string> tokenize = Tokenizer.Tokenize(Normalize);
+                        expression_tree tree = new expression_tree(tokenize);
+                        node root = tree.Parse();
+                        double result = evaluator.Evaluate(root, vars);
+                        Console.WriteLine("Result = " + result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(" ==> " + ex.Message);
+                    }
                 }
-            }
-            if (n == 3)
-            {
-                try
+                break;
+            case 3:
                 {
-                    Console.WriteLine("Enter the Priority from 1 to 3: ");
-                    Console.Write("Enter the Add_Sub: "); //
-                    int add_sum = int.Parse(Console.ReadLine());
-                    Console.Write("Enter the Mul_Div: ");
-                    int mul_div = int.Parse(Console.ReadLine()); //
-                    Console.Write("Enter the Pow_Rad: ");
-                    int pow_rad = int.Parse(Console.ReadLine()); //
-                    Console.Write("Enter Input : ");
-                    string? infix = Console.ReadLine();
-                    string Normalize = Preprocess.Normalize(infix);
-                    List<string> tokenize = Tokenizer.Tokenize(Normalize);
-                    expression_tree parser = new expression_tree(tokenize);
-                    parser.SetPrecedence(add_sum, mul_div, pow_rad);
-                    node root = parser.Parse();
-                    double result = evaluator.Evaluate(root);
-                    Console.WriteLine("Result = " + result);
+                    try
+                    {
+                        Console.WriteLine("Enter the Priority from 1 to 3: ");
+                        Console.Write("Enter the Add_Sub: "); //
+                        int add_sum = int.Parse(Console.ReadLine());
+                        Console.Write("Enter the Mul_Div: ");
+                        int mul_div = int.Parse(Console.ReadLine()); //
+                        Console.Write("Enter the Pow_Rad: ");
+                        int pow_rad = int.Parse(Console.ReadLine()); //
+                        Console.Write("Enter Input : ");
+                        string? infix = Console.ReadLine();
+                        string Normalize = Preprocess.Normalize(infix);
+                        List<string> tokenize = Tokenizer.Tokenize(Normalize);
+                        expression_tree parser = new expression_tree(tokenize);
+                        parser.SetPrecedence(add_sum, mul_div, pow_rad);
+                        node root = parser.Parse();
+                        double result = evaluator.Evaluate(root);
+                        Console.WriteLine("Result = " + result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(" ==> " + ex.Message);
+                    }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(" ==> " + ex.Message);
-                }
+                break;
             }
         }
     }
